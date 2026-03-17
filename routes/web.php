0fe81d24afee873b1c782e7bba7b6a9f76ddb93c
Lê Trần Trọng Luật <trluat@MacBook-Air-TrLuat.local>
2026-03-17 09:15:02 +0700

    hello

--- routes/web.php
+++ routes/web.php
@@ -174,3 +174,5 @@ Route::get('session/delete', [SessionController::class,'delete']);
 
 Route::get('cookie/set', [CookieController::class,'set']);
 Route::get('cookie/get', [CookieController::class,'get']);
+
+Route::get("/letrantrongluat","App\Http\Controllers\phplaravel@inten");
